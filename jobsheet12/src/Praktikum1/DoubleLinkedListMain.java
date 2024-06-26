c26e5d097949ff58c923e02b9bc6c5708b98f997
nathanaelGracedo <nathanjuan56@gmail.com>
2024-06-26 19:17:11 +0700

    praktikum 3

--- jobsheet12/src/Praktikum1/DoubleLinkedListMain.java
+++ jobsheet12/src/Praktikum1/DoubleLinkedListMain.java
@@ -39,6 +39,20 @@ public class DoubleLinkedListMain {
             dll.print();
             System.out.println("Size: " + dll.size());
             System.out.println("=============================");
+            dll.clear();
+
+            dll.addFirst(3);
+            dll.addLast(4);
+            dll.addFirst(7);
+            dll.print();
+            System.out.println("Size: " + dll.size());
+            dll.add(40, 1);
+            dll.print();
+            System.out.println("Size: " + dll.size());
+            System.out.println("=============================");
+            System.out.println("Data awal pada linked list adalah:" + dll.getFirst());
+            System.out.println("Data akhirr pada linked list adalah:" + dll.getLast());
+            System.out.println("Data indeks ke-1 pada linked list adalah:" + dll.get(1));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
