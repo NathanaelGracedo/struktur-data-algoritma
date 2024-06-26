e5a94e2a28a5d9e4034d59cb03f9e0b20848c820
nathanaelGracedo <nathanjuan56@gmail.com>
2024-06-26 18:37:20 +0700

    praktikum2

--- jobsheet12/src/Praktikum1/DoubleLinkedList.java
+++ jobsheet12/src/Praktikum1/DoubleLinkedList.java
@@ -42,14 +42,14 @@ public class DoubleLinkedList {
         if (isEmpty()) {
             addFirst(item);
         } else if (index < 0 || index > size) {
-            throw new Exception("Nilai indeks diluarr batas");
+            throw new Exception("Nilai indeks diluar batas");
         } else {
             if (index == 0) {
                 addFirst(item);
             } else {
                 Node current = head;
                 int i = 0;
-                while (i < index -1) {
+                while (i < index - 1) {
                     current = current.next;
                     i++;
                 }
@@ -77,14 +77,75 @@ public class DoubleLinkedList {
 
     public void print() {
         if (isEmpty()) {
+            System.out.println("Linked List kosong");
+        } else {
             Node tmp = head;
             while (tmp != null) {
                 System.out.print(tmp.data + "\t");
                 tmp = tmp.next;
             }
             System.out.println("\nBerhasil diisi");
+        }
+    }
+
+    public void removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong, tidak dapat dihapus");
         } else {
-            System.out.println("Linked List kosong");
+            if (head.next == null) {
+                head = null;
+            } else {
+                head = head.next;
+                head.prev = null;
+            }
+            size--;
+        }
+    }
+
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong, tidak dapat dihapus");
+        }
+        if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+
+        current.next = null;
+        size--;
+    }
+
+    public void remove(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas");
+        }
+
+        if (index == 0) {
+            this.removeFirst();
+        } else {
+            Node current = this.head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.next == null) {
+                current.prev.next = null;
+            } else if (current.prev == null) {
+                current = current.next;
+                current.prev = null;
+                head = current;
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+            size--;
         }
     }
 }
--- jobsheet12/src/Praktikum1/DoubleLinkedListMain.java
+++ jobsheet12/src/Praktikum1/DoubleLinkedListMain.java
@@ -20,6 +20,25 @@ public class DoubleLinkedListMain {
             dll.clear();
             dll.print();
             System.out.println("Size: " + dll.size());
+            dll.addLast(50);
+            dll.addLast(40);
+            dll.addLast(10);
+            dll.addLast(20);
+            dll.print();
+            System.out.println("Size: " + dll.size());
+            System.out.println("=============================");
+            dll.removeFirst();
+            dll.print();
+            System.out.println("Size: " + dll.size());
+            System.out.println("=============================");
+            dll.removeLast();
+            dll.print();
+            System.out.println("Size: " + dll.size());
+            System.out.println("=============================");
+            dll.remove(1);
+            dll.print();
+            System.out.println("Size: " + dll.size());
+            System.out.println("=============================");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
