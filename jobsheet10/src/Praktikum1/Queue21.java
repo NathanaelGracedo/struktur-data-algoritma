c2c51d3454f33a4037dc653073e847b97e2501e0
nathanaelGracedo <nathanjuan56@gmail.com>
2024-06-26 12:24:56 +0700

    percobaan 1

---
+++ jobsheet10/src/Praktikum1/Queue21.java
@@ -0,0 +1,103 @@
+package Praktikum1;
+
+import javax.swing.text.StyledEditorKit;
+
+public class Queue21 {
+    int[] data;
+    int front;
+    int rear;
+    int size;
+    int max;
+
+    public void Queue(int n) {
+        max = n;
+        data = new int[max];
+        size = 0;
+        front = rear = -1;
+   }
+
+   public boolean IsEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+        return false;
+        }
+   }
+
+   public boolean IsFull() {
+        if (size == max) {
+            return true;
+        } else {
+            return false;
+        }
+   }
+
+   public void peek() {
+        if (!IsEmpty()) {
+            System.out.println("Elemen terdepan: " + data[front]);
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+   }
+
+   public void print() {
+        if (IsEmpty()) {
+            System.out.println("Queue masih kosong");
+        } else {
+            int i = front;
+            while (i != rear) {
+                System.out.print(data[i] + " ");
+                i = (i +1) % max;
+            }
+            System.out.println(data[i] + " ");
+            System.out.println("Jumlah elemen = " + size);
+        }
+   }
+
+   public void clear() {
+        if (!IsEmpty()) {
+            front = rear = -1;
+            size = 0;
+            System.out.println("Queue berhasil dikosongkan");
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+   }
+
+   public void Enqueue(int dt) {
+        if (IsFull()) {
+            System.out.println("Queue sudah penuh");
+            System.exit(1);
+        } else {
+            if (IsEmpty()) {
+                front = rear = 0;
+            } else {
+                if (rear == max - 1) {
+                    rear = 0;
+                } else {
+                    rear++;
+                }
+            }
+            data[rear] = dt;
+            size++;
+        }
+   }
+
+    11
+        } else {
+            dt = data[front];
+            size--;
+            if (IsEmpty()) {
+                front = rear = -1;
+            } else {
+                if (front == max - 1) {
+                    front = 0;
+                } else {
+                    front++;
+                }
+            }
+        }
+        return dt;
+    }
+
+}
